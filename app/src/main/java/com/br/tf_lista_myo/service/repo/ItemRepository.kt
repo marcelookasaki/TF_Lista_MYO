a5a7724e54bc5fa853d863cbf741d3fdd2c2fdc9
marcelookasaki <marcelo.okasaki@gmail.com>
2022-04-12 19:59:12 -0300

    Singleton implementado

--- app/src/main/java/com/br/tf_lista_myo/service/repo/ItemRepository.kt
+++ app/src/main/java/com/br/tf_lista_myo/service/repo/ItemRepository.kt
@@ -1,13 +1,34 @@
 package com.br.tf_lista_myo.service.repo
 
+import android.content.Context
 import com.br.tf_lista_myo.service.model.ItemModel
 
-class ItemRepository {
+class ItemRepository private constructor(context: Context){
 
-    /* Create Read Update Delete */
+    /**
+     * Singleton → método estático responsável por dar apenas
+     * uma instância da classe ItemRepository
+     **/
+
+    private var myItemDBHelper: ItemDBHelper = ItemDBHelper(context)
+
+    companion object {
+
+        private lateinit var repositorio: ItemRepository
+
+        fun getInstance(context: Context) : ItemRepository {
+
+            if (!::repositorio.isInitialized ) {
+                repositorio = ItemRepository(context)
+            }
+            return ItemRepository(context)
+
+        }
+
+    }
 
     fun save(item: ItemModel) {
-
+        myItemDBHelper.writableDatabase
     }
 
     fun getAll(): List<ItemModel> {
--- app/src/main/java/com/br/tf_lista_myo/view_model/ItemFormViewModel.kt
+++ app/src/main/java/com/br/tf_lista_myo/view_model/ItemFormViewModel.kt
@@ -1,14 +1,19 @@
 package com.br.tf_lista_myo.view_model
 
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.br.tf_lista_myo.service.model.ItemModel
 import com.br.tf_lista_myo.service.repo.ItemRepository
 
-class ItemFormViewModel : ViewModel() {
+class ItemFormViewModel(application: Application) : AndroidViewModel(application){
 
-    private val myItemRepository : ItemRepository = ItemRepository()
+    private val myContext = application.applicationContext
+    private val myItemRepository : ItemRepository = ItemRepository.getInstance(myContext)
+
 
     /* Live data dos itens */
     private var mySaveItem = MutableLiveData<Boolean>()
